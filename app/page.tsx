07896d78b5dbbdb5574765dcd099fa8099d7b441
Michael Mercer <michael.mercer@hey.com>
2023-08-28 13:15:49 -0700

    Update copy

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,9 @@ export default function Home() {
           <a href="https://kairos.art" target="_blank">
             Kairos
           </a>{' '}
-          bring brands into the web3 space. Previously, I was the Director of
-          Web Technology at{' '}
+          Bring radical participation to web3 by building word-class NFT tools
+          for artists and businesses. Previously, I was the Director of Web
+          Technology at{' '}
           <a href="https://thestarrconspiracy.com" target="_blank">
             The Starr Conspiracy
           </a>{' '}
